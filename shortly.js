ac52e94cbb5446c4718eeb8c7b651f90147dcdf8
GMeyr <gabrielmeyr@gmail.com>
2015-12-28 19:37:59 -0500

    created rough draft users table and model

--- shortly.js
+++ shortly.js
@@ -23,32 +23,34 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/', 
+app.get('/',
 function(req, res) {
   res.render('index');
 });
 
-app.get('/create', 
+app.get('/create',
 function(req, res) {
   res.render('index');
 });
 
-app.get('/links', 
+app.get('/links',
 function(req, res) {
   Links.reset().fetch().then(function(links) {
     res.send(200, links.models);
   });
 });
 
-app.post('/links', 
+app.post('/links',
 function(req, res) {
   var uri = req.body.url;
-
+  // console.log(uri, 'uri in app.post');
+  // console.log(res.body, 'res.body in app.post');
+  // console.log(util.isValidUrl(uri), 'uri is valid function result in app.post');
   if (!util.isValidUrl(uri)) {
     console.log('Not a valid url: ', uri);
     return res.send(404);
   }
-
+  console.log("url is valid");
   new Link({ url: uri }).fetch().then(function(found) {
     if (found) {
       res.send(200, found.attributes);
